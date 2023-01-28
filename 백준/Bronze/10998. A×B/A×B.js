2ea23967a2f174fb12dd506c2ee90f021ab2cbb9
seungyeon-rr <106301980+seungyeon-rr@users.noreply.github.com>
2023-01-29 00:16:12 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+var input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a*b);
