275ba834065199689986ffb46b01251c140e563c
Ryooooo <106301980+seungyeon-rr@users.noreply.github.com>
2023-02-03 08:44:19 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,8 @@
+let fs = require("fs")
+let input = fs.readFileSync("/dev/stdin").toString()
+
+if ((input % 4 == 0 && input % 100 !== 0) || input % 400 == 0) {
+    console.log(1)
+} else {
+    console.log(0)
+}
