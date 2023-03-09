03f7e650975c46166423ce84e185a6077e4ed684
Ryooooo <106301980+seungyeon-rr@users.noreply.github.com>
2023-03-09 23:47:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = true;
+    if (!(s.length == 4 || s.length ==  6)){
+        return false
+    } else {
+        const tmp = s.split('')
+        for(let i =0;i<tmp.length;i++){
+            if (isNaN(tmp[i])){
+                return false
+            }
+        }
+
+    }
+    return answer;
+}
