ee5f61576e168ba12bd74996eca813697c9ee53b
Ryooooo <106301980+seungyeon-rr@users.noreply.github.com>
2024-10-01 15:23:36 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.27 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    
+    for (let x = 2; x < n; x++) {
+        if (n % x === 1) {
+            return answer = x;
+        }
+    }
+    return answer;
+}
